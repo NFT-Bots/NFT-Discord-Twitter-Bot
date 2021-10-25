6df8184141c613ec430b8830b018bc1e4a92d757
nftbots <info@nft-bots.i>
2021-10-25 23:20:30 +0200

    added Readme and updated code

--- README.md
+++ README.md
@@ -1,16 +1,181 @@
-# NFT Sales/Listing Twitter and Discord Bot
-NFT bot that monitors sales and listings for a given collection & then posts them to Discord and Twitter (optional for sales).
-Support at https://nft-bots.io
+<!-- TABLE OF CONTENTS -->
+<details>
+  <summary>Table of Contents</summary>
+  <ol>
+    <li>
+      <a href="#about-the-project">About The Project</a>
+      <ul>
+        <li><a href="#built-with">Built With</a></li>
+      </ul>
+    </li>
+    <li>
+      <a href="#getting-started">Getting Started</a>
+      <ul>
+        <li><a href="#prerequisites">Prerequisites</a></li>
+        <li><a href="#installation">Installation</a></li>
+      </ul>
+    </li>
+    <li><a href="#usage">Usage</a></li>
+    <li><a href="#roadmap">Roadmap</a></li>
+    <li><a href="#contributing">Contributing</a></li>
+    <li><a href="#license">License</a></li>
+    <li><a href="#contact">Contact</a></li>
+    <li><a href="#acknowledgments">Acknowledgments</a></li>
+  </ol>
+</details>
 
-## Installation
-```sh
-npm install
-```
-## Config vars
-```sh
-Checkout config.js
-```
-## Run
-```sh
-node app.js
-```
+
+<!-- ABOUT THE PROJECT -->
+## About The Project
+
+[![Product Name Screen Shot][product-screenshot]](https://nft-bots.io)
+
+This Repository was created by the NFT-Bot-Team and hosts some sample code on how to retrieve Sales and Listings Events from Opensea.
+The idea was to create a easy to read and understand code, which can be used to build your own Project around NFT Sales and Listings events.
+
+Please let us know if you need support with the code on through our homepage and our Discord Channel.
+
+If you feel like not investing to much into self coding your NFT Sales and Listings bot for all the marketplaces out there feel free to get in
+touch with us on our [Homepage](https://nft-bots.io).
+
+We've already built a bot Monitoring all Sales and Listings Events for most NFT Marketplaces out there:
+* Opensea
+* Solanart
+* Magiceden
+* Alpha.art
+
+Further we are also able to send those Events to Discord and your desired Twitter Account to increase your Awareness on social media. Just join our Discord Server and let us know about your question and how we can help you to scale your NFT Collection to the next level.
+<p align="right">(<a href="#top">back to top</a>)</p>
+
+
+
+### Built With
+
+This repostirory includes some ready to run sample code to retrieve all Opensea Listing and Sales event in a defined intervall. It was build with node.js including a few usefull libraries.
+
+* [node.js](https://nodejs.org/en/)
+
+<p align="right">(<a href="#top">back to top</a>)</p>
+
+
+
+<!-- GETTING STARTED -->
+## Getting Started
+
+This is a short step by step tutorial on how you can get this code up and running on your local server.
+
+### Installation
+
+1. Install npm
+  ```sh
+  npm install npm@latest -g
+  ```
+2. Clone the repo
+   ```sh
+   git clone https://github.com/NFT-Bots/NFT-Discord-Twitter-Bot
+   ```
+3. Install NPM packages
+   ```sh
+   npm install
+   ```
+4. Run the Code
+   ```sh
+   node apps.js
+   ```
+
+### Adopt the code to your needs
+* Checkout config.js to make your adjusments such as intervall time
+* Spot the "// here comes your code" in app.js, you can do anything with the Sales and Listings Events there
+
+<p align="right">(<a href="#top">back to top</a>)</p>
+
+<!-- USAGE EXAMPLES -->
+## Usage
+
+Run the code and be the first to know about new Sales or Listings Events for your favorite collection. Marketplaces usually take about 30seconds to post those Events on their Homepage.
+
+_For more info, please refer to our [Homepage](https://nft-bots.io)_
+
+<p align="right">(<a href="#top">back to top</a>)</p>
+
+
+
+<!-- ROADMAP -->
+## Roadmap
+Here is the Road of our NFT-Bot services, some of this code may be included in this Opensource repository here:
+- [x] Posting Sales and Listings events to Discord
+- [x] Posting Sales events to Twitter
+- [x] Adding customization features for Discord appearance and Twitter message
+- [x] Support Solana NFT Marketplaces: solanart.io, magiceden.io, Alpha.art
+- [] Adding a floor price monitor
+- [] Adding collection stats to be displayed in Discord
+- [] Add Digital Eyes as a new marketplace
+
+<p align="right">(<a href="#top">back to top</a>)</p>
+
+
+
+<!-- CONTRIBUTING -->
+## Contributing
+
+Contributions are what make the open source community such an amazing place to learn, inspire, and create. Any contributions you make are **greatly appreciated**.
+
+If you have a suggestion that would make this better, please fork the repo and create a pull request. You can also simply open an issue with the tag "enhancement".
+Don't forget to give the project a star! Thanks again!
+
+1. Fork the Project
+2. Create your Feature Branch (`git checkout -b feature/AmazingFeature`)
+3. Commit your Changes (`git commit -m 'Add some AmazingFeature'`)
+4. Push to the Branch (`git push origin feature/AmazingFeature`)
+5. Open a Pull Request
+
+<p align="right">(<a href="#top">back to top</a>)</p>
+
+
+
+<!-- LICENSE -->
+## License
+
+Distributed under the MIT License. See `LICENSE.txt` for more information.
+
+<p align="right">(<a href="#top">back to top</a>)</p>
+
+
+
+<!-- CONTACT -->
+## Contact
+[Join our Discord](https://discord.com/invite/heTBaMvJyU)
+
+Project Link: [https://github.com/NFT-Bots/NFT-Discord-Twitter-Bot](https://github.com/NFT-Bots/NFT-Discord-Twitter-Bot)
+
+<p align="right">(<a href="#top">back to top</a>)</p>
+
+
+
+<!-- ACKNOWLEDGMENTS -->
+## Acknowledgments
+* [NFT-Bot Services for Discord and Twitter](https://nft-bots.io)
+* [MIT License](https://opensource.org/licenses/MIT)
+* [GitHub Emoji Cheat Sheet](https://www.webpagefx.com/tools/emoji-cheat-sheet)
+* [GitHub Pages](https://pages.github.com)
+* [Font Awesome](https://fontawesome.com)
+* [React Icons](https://react-icons.github.io/react-icons/search)
+* [Readme Template](https://github.com/othneildrew/Best-README-Template/blob/master/README.md)
+
+<p align="right">(<a href="#top">back to top</a>)</p>
+
+<!-- MARKDOWN LINKS & IMAGES -->
+<!-- https://www.markdownguide.org/basic-syntax/#reference-style-links -->
+[contributors-shield]: https://img.shields.io/github/contributors/othneildrew/Best-README-Template.svg?style=for-the-badge
+[contributors-url]: https://github.com/othneildrew/Best-README-Template/graphs/contributors
+[forks-shield]: https://img.shields.io/github/forks/othneildrew/Best-README-Template.svg?style=for-the-badge
+[forks-url]: https://github.com/othneildrew/Best-README-Template/network/members
+[stars-shield]: https://img.shields.io/github/stars/othneildrew/Best-README-Template.svg?style=for-the-badge
+[stars-url]: https://github.com/othneildrew/Best-README-Template/stargazers
+[issues-shield]: https://img.shields.io/github/issues/othneildrew/Best-README-Template.svg?style=for-the-badge
+[issues-url]: https://github.com/othneildrew/Best-README-Template/issues
+[license-shield]: https://img.shields.io/github/license/othneildrew/Best-README-Template.svg?style=for-the-badge
+[license-url]: https://github.com/othneildrew/Best-README-Template/blob/master/LICENSE.txt
+[linkedin-shield]: https://img.shields.io/badge/-LinkedIn-black.svg?style=for-the-badge&logo=linkedin&colorB=555
+[linkedin-url]: https://linkedin.com/in/othneildrew
+[product-screenshot]: https://nft-bots.io/wp-content/uploads/2021/10/home-sales-2.jpg
--- app.js
+++ app.js
@@ -1,8 +1,71 @@
+// required modules
 let _ = require('lodash');
 let moment = require('moment');
 let config = require('./config');
 let axios = config.axios;
 
+// get opensea events
+async function get_os_events(slug, mode){
+    let searchBack = config.globals.search_back_min;
+    let occurred_after = moment().startOf('minute').subtract(searchBack, "minutes").unix();
+    let event_type_string = null;
+
+    // mode is defining if Sales or Listings events should be retrieved
+    if( mode == "sales" ){
+        event_type_string = 'successful';
+    } else if( mode == "listings" ){
+        event_type_string = 'created';
+    } else {
+        console.log(`ERROR ${slug}: Invalid mode specified: ${mode}`)
+        return "invalidMode"
+    }
+
+    // get all Listings Events from Opensea
+    axios.get('https://api.opensea.io/api/v1/events', {
+        params: {
+            collection_slug: slug,
+            event_type: event_type_string,
+            occurred_after: occurred_after,
+            only_opensea: 'true'
+        }
+    }).then((response) => {
+        let events = _.get(response, ['data', 'asset_events']);
+        console.log(`${slug}: ${events.length} Listings in the last ${searchBack} seconds.`);
+
+        // parsing Listings Events
+        _.each(events, (event) => {
+            return process_os_event(event, mode)
+        });
+    }).catch((error) => {
+        console.error(error);
+    });
+}
+
+async function process_os_event(event, mode){
+    // extract data from opensea Event
+    let asset_token_id = _.get(event, ['asset', 'token_id']);
+    let event_id = _.get(event, ['id']);
+    let collection_slug = _.get(event, ['collection_slug']);
+    let tokenName = _.get(event, ['asset', 'name']);
+    let image = _.get(event, ['asset', 'image_url']);
+    let openseaLink = _.get(event, ['asset', 'permalink']);
+    let seller = _.get(event, ['seller', 'address']);
+    let usdValue = Number(_.get(event, ['payment_token', 'usd_price'])).toFixed(2);
+
+    // check if necessary fields are defined
+    if (event_id === undefined || asset_token_id === undefined ){
+        return "UndefinedEvent";
+    }
+    // generate tokenName if not defined in Event
+    if (tokenName == null) tokenName = `${collection_slug}_${asset_token_id}`;
+
+    // logging Event Details
+    console.log(`INFO ${collection_slug}: New ${mode} event found. ID: ${asset_token_id}, Token: ${tokenName}, ImageUrl: ${image}, OS: ${openseaLink}, Seller: ${seller}, Price: ${usdValue}$`);
+
+    // here comes your code
+}
+
+// main loop
 setInterval(() => {
     // slug config to be checked
     let sluglist = [ "thecryptomoms", "topdogbeachclub", "cryptocannabisclub", "punkscapes" ];
@@ -11,96 +74,9 @@ setInterval(() => {
         let searchBack = config.globals.search_back_min;
         let occurred_after = moment().startOf('minute').subtract(searchBack, "minutes").unix();
 
-        // get all Listings Events from Opensea
-        axios.get('https://api.opensea.io/api/v1/events', {
-            params: {
-                collection_slug: slug,
-                event_type: 'created',
-                occurred_after: occurred_after,
-                only_opensea: 'true'
-            }
-        }).then((response) => {
-            let events = _.get(response, ['data', 'asset_events']);
-            console.log(`${slug}: ${events.length} Listings in the last ${searchBack} seconds.`);
-
-            // parsing Listings Events
-            _.each(events, (event) => {
-                // extract data from opensea Event
-                let asset_token_id = _.get(event, ['asset', 'token_id']);
-                let event_id = _.get(event, ['id']);
-                let totalPrice = _.get(event, 'starting_price');
-                let collection_slug = _.get(event, ['collection_slug']);
-                let tokenName = _.get(event, ['asset', 'name']);
-                let image = _.get(event, ['asset', 'image_url']);
-                let openseaLink = _.get(event, ['asset', 'permalink']);
-                let seller = _.get(event, ['seller', 'user', 'username']) || _.get(event, ['seller', 'address']);
-                let usdValue = _.get(event, ['payment_token', 'usd_price']);
-                let tokenSymbol = _.get(event, ['payment_token', 'symbol']);
-
-                // check if necessary fields are defined
-                if (event_id === undefined || asset_token_id === undefined ){
-                    return "UndefinedEvent";
-                }
-                if (slug != collection_slug){
-                    return "EventSlugInvalid";
-                }
-                // generate tokenName if not defined in Event
-                if (tokenName == null) tokenName = `${collection_slug}_${asset_token_id}`;
-
-                // logging Event Details
-                console.log(`INFO ${slug}: New Sales Event found. ID: ${asset_token_id}, Token: ${tokenName}, ImageUrl: ${image}, OS: ${openseaLink}, Seller: ${seller}, Price: ${totalPrice}`);
-
-                // here comes your code
-            });
-        }).catch((error) => {
-            console.error(error);
-        });
-
-        // get all Sales Events from Opensea
-        axios.get('https://api.opensea.io/api/v1/events', {
-            params: {
-                collection_slug: slug,
-                event_type: 'successful',
-                occurred_after: occurred_after,
-                only_opensea: 'true'
-            }
-        }).then((response) => {
-            let events = _.get(response, ['data', 'asset_events']);
-            console.log(`${slug}: ${events.length} Sales in the last ${searchBack} seconds.`);
-
-            // parsing Sales Events
-            _.each(events, (event) => {
-                // extract data from opensea Event
-                let asset_token_id = _.get(event, ['asset', 'token_id']);
-                let event_id = _.get(event, ['id']);
-                let totalPrice = _.get(event, 'total_price');
-                let collection_slug = _.get(event, ['collection_slug']);
-                let tokenName = _.get(event, ['asset', 'name']);
-                let image = _.get(event, ['asset', 'image_url']);
-                let openseaLink = _.get(event, ['asset', 'permalink']);
-                let seller = _.get(event, ['seller', 'user', 'username']) || _.get(event, ['seller', 'address']);
-                let usdValue = _.get(event, ['payment_token', 'usd_price']);
-                let tokenSymbol = _.get(event, ['payment_token', 'symbol']);
-
-                // check if necessary fields are defined
-                if (event_id === undefined || asset_token_id === undefined ){
-                    return "UndefinedEvent";
-                }
-                if (slug != collection_slug){
-                    return "EventSlugInvalid";
-                }
-                // generate tokenName if not defined in Event
-                if (tokenName == null) tokenName = `${collection_slug}_${asset_token_id}`;
-
-                // logging Event Details
-                console.log(`INFO ${slug}: New Sales Event found. ID: ${asset_token_id}, Token: ${tokenName}, ImageUrl: ${image}, OS: ${openseaLink}, Seller: ${seller}, Price: ${totalPrice}`);
-
-                // here comes your code
-            });
-        }).catch((error) => {
-            console.error(error);
-        });
+        // now process Sales and Listings Events for the above sluglist
+        get_os_events(slug, "sales")
+        get_os_events(slug, "listings")
     }
-
 // Intervall in ms
 }, Number(config.globals.interval_time_ms) );
